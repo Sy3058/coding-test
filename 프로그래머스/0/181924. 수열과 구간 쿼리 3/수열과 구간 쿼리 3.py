164e7e7c586904185c43500e67030d6749ba1d15
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2024-01-18 15:15:11 +0900

    Update 수열과 구간 쿼리 3.py

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -2,3 +2,5 @@ def solution(arr, queries):
     for i in queries:
         arr[i[0]], arr[i[1]] = arr[i[1]], arr[i[0]]
     return arr
+
+# 리스트의 i번째 요소와 j번째 요소의 값을 변경하려면 arr[i], arr[j] = arr[j], arr[i] 하면 됨
