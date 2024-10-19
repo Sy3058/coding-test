2e0f3a592b93323aa3fd03a3287af0e59da05c8d
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2024-10-19 23:33:17 +0900

    [Bronze II] Title: 방 배정, Time: 32 ms, Memory: 33240 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,18 @@
+import math
+
+n, k = map(int, input().split()) # 수학여행에 참가하는 학생 수, 한 방에 배정할 수 있는 최대 인원 수
+girls = [0 for _ in range (6)] # 여학생 리스트
+boys = [0 for _ in range (6)] # 남학생 리스트
+
+for _ in range (n):
+  s, y = map(int, input().split()) # s: 여학생 0, 남학생 1, y: 학년
+  if s == 0: # 여학생인 경우
+    girls[y-1] += 1 # 인덱스이므로 학년 -1
+  else:
+    boys[y-1] += 1
+
+for i in range (6):
+  girls[i] = math.ceil(girls[i]/2) # 한 명만 있어도 방을 배정해야 하므로 올림
+  boys[i] = math.ceil(boys[i]/2)
+
+print(sum(girls)+sum(boys))
