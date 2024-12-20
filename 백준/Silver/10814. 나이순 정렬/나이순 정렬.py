4745892bbfe6d32c6d4f1ef5b7f4cd06f58219de
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2024-12-21 01:57:57 +0900

    [Silver V] Title: 나이순 정렬, Time: 212 ms, Memory: 46020 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+
+for _ in range (n):
+  age, name = sys.stdin.readline().split()
+  arr.append((int(age), name))
+
+arr.sort(key = lambda x:x[0])
+for age, name in arr:
+  print(age, name)
