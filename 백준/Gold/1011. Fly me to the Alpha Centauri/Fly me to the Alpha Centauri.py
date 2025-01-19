38df3d55a8a59f67e23b8214a932dec7a2162166
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-01-19 22:15:22 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range (t):
+  x, y = map(int, input().split())
+  dis = y - x
+  n = int(dis ** 0.5)
+  while n**2 + n > dis:
+    n -= 1
+
+  rest = dis - (n**2 + n)
+
+  if rest == 0:
+    print(2*n)
+
+  elif rest <= n+1:
+    print(2*n + 1)
+
+  else:
+    print(2*n + 2)
