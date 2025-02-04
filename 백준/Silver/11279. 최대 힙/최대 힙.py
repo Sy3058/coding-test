fdc2e8241d30990d37417a67c67c1d8cfa4b42e9
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-02-04 15:56:09 +0900

    [Silver II] Title: 최대 힙, Time: 108 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+h = []
+for _ in range (n):
+  x = int(input())
+
+  if x == 0:
+    if h:
+      print(-heapq.heappop(h))
+    else:
+      print(0)
+  else:
+    heapq.heappush(h, -x) # 가장 작은 수부터 출력하므로 부호를 반대로 해야 최대값부터 출력 가능
