86a36dd469efa5dd3f312ad458d4b63ea762db7b
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-02-12 03:04:54 +0900

    [Silver IV] Title: 평행사변형, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1064. 평행사변형/평행사변형.py
+++ 백준/Silver/1064. 평행사변형/평행사변형.py
@@ -5,17 +5,8 @@ x1, y1, x2, y2, x3, y3 = map(int, input().split())
 
 def check():
   # 세 점이 한 직선에 있는 경우
-  if x1 == x2 == x3 or y1 == y2 == y3:
+  if (y2-y1)*(x3-x2) == (y3-y2)*(x2-x1):
     return -1.0
-  if x1 == x2:
-    if (y3-y1)/(x3-x1) == (y3-y2)/(x3-x2):
-      return -1.0
-  elif x2 == x3:
-    if (y3-y1)/(x3-x1) == (y2-y1)/(x2-x1):
-      return -1.0
-  else:
-    if (y3-y2)/(x3-x2) == (y2-y1)/(x2-x1):
-      return -1.0
   ab = ((x2-x1)**2 + (y2-y1)**2)**0.5
   bc = ((x3-x2)**2 + (y3-y2)**2)**0.5
   ac = ((x3-x1)**2 + (y3-y1)**2)**0.5
