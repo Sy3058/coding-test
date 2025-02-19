899296c4dac2ade8c1b0e0384600ebcbbbb57a73
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-02-20 02:21:13 +0900

    [Silver V] Title: 막대기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -2,19 +2,15 @@ import sys
 input = sys.stdin.readline
 
 x = int(input())
-rod = [0, 0, 0, 0, 0, 0, 0]
-rod[0] = x//64
-x %= 64
-rod[1] = x//32
-x %= 32
-rod[2] = x//16
-x %= 16
-rod[3] = x//8
-x %= 8
-rod[4] = x//4
-x %= 4
-rod[5] = x//2
-x %= 2
-rod[6] = x//1
+stick = [64, 32, 16, 8, 4, 2, 1]
+cnt = 0
 
-print(rod.count(1))
+for i in range (len(stick)):
+  if x >= stick[i]:
+    x -= stick[i]
+    cnt += 1
+  
+  if x == 0:
+    break
+
+print(cnt)
