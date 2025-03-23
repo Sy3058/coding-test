735bb009f2fd66424e2d2e26dfa44aa67bf7629d
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-03-23 22:46:46 +0900

    [Silver I] Title: 카잉 달력, Time: 2164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,44 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-def gcd(a, b):
-  while b != 0:
-    r = a%b
-    a, b = b, r
-  return a
+def sol(m, n, x, y):
+  k = x
+  while k <= m * n:
+    if (k-x) % m == 0 and (k-y) % n == 0:
+      return k
+    k += m
+  return -1
 
 t = int(input())
-
 for _ in range (t):
   m, n, x, y = map(int, input().split())
-
-  lcm = m*n // gcd(m,n) # least common multiple
-  i = 0
-  flag = False
-
-  if x > y:
-    while n*i + y <= lcm:
-      if (n*i) % m == x-y:
-        print(n*i + y)
-        flag = True
-        break
-      i += 1
-  
-  elif x < y:
-    while m*i + x <= lcm:
-      if (m*i) % n == y-x:
-        print(m*i + x)
-        flag = True
-        break
-      i += 1
-  
-  else:
-    while (m*i) + x <= lcm:
-      if (m*i) % n == 0:
-        print(m*i + x)
-        flag = True
-        break
-      i += 1
-  
-  if not flag:
-    print(-1)
+  print(sol(m, n, x, y))
