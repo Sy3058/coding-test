991528e0405ec216dc45ecfd719ed984d2c67056
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-03-29 01:48:49 +0900

    [Silver I] Title: 봄버맨, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -5,41 +5,43 @@ r, c, n = map(int, input().split())
 board = [list(input().strip()) for _ in range(r)]
 directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-if n <= 1:  # 초기 상태 출력
-    for row in board:
-        print(''.join(row))
+if n <= 1: # initial state
+  for row in board:
+    print(''.join(row))
 
-elif n % 2 == 0:  # 모든 칸에 폭탄이 있는 상태 출력
-    for _ in range(r):
-        print('O' * c)
+elif n % 2 == 0: # board is full of bomb
+  for _ in range (r):
+    print('O' * c)
 
 else:
-    # 첫 번째 폭발 이후 상태 계산
-    e1 = [['O'] * c for _ in range(r)]
-    for i in range(r):
-        for j in range(c):
-            if board[i][j] == 'O':
-                e1[i][j] = '.'
-                for di, dj in directions:
-                    ni, nj = i + di, j + dj
-                    if 0 <= ni < r and 0 <= nj < c:
-                        e1[ni][nj] = '.'
+  # state after first explosion
+  ep1 = [['O'] * c for _ in range (r)]
+  for i in range (r):
+    for j in range (c):
+      if board[i][j] == 'O': # bomb refer to initial state
+        ep1[i][j] = '.' # bomb explodes
 
-    # 두 번째 폭발 이후 상태 계산
-    e2 = [['O'] * c for _ in range(r)]
-    for i in range(r):
-        for j in range(c):
-            if e1[i][j] == 'O':
-                e2[i][j] = '.'
-                for di, dj in directions:
-                    ni, nj = i + di, j + dj
-                    if 0 <= ni < r and 0 <= nj < c:
-                        e2[ni][nj] = '.'
+        for di, dj in directions:
+          ni, nj = i + di, j + dj
+          if 0 <= ni < r and 0 <= nj < c:
+            ep1[ni][nj] = '.' # nearby areas explode
+  
+  # state after second explosion
+  ep2 = [['O'] * c for _ in range (r)]
+  for i in range (r):
+    for j in range (c):
+      if ep1[i][j] == 'O': # bomb refer to ep1
+        ep2[i][j] = '.'
 
-    # 시간 n에 따른 출력
-    if n % 4 == 3:
-        for row in e1:
-            print(''.join(row))
-    elif n % 4 == 1:
-        for row in e2:
-            print(''.join(row))
+        for di, dj in directions:
+          ni, nj = i + di, j + dj
+          if 0 <= ni < r and 0 <= nj < c:
+            ep2[ni][nj] = '.'
+  
+  if n % 4 == 3:
+    for row in ep1:
+      print(''.join(row))
+  
+  elif n % 4 == 1:
+    for row in ep2:
+      print(''.join(row))
