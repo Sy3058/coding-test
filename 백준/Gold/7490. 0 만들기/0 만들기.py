a6315e978acb25735fc533de2f4f3999f52de433
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-04-02 15:38:10 +0900

    [Gold V] Title: 0 만들기, Time: 148 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -1,32 +1,19 @@
 import sys
-import itertools
 input = sys.stdin.readline
 
-t = int(input()) # test case
-co = ('', '+', '-') # operators that can be selected (in ASCII code order)
+def dfs(cf, num): # calculation formula, number
+  if num > n:
+    if eval(cf.replace(" ", "")) == 0: # replace " " to calculate string
+      print(cf)
+  
+  else:
+    # in ASCII code order
+    dfs(f"{cf} {num}", num + 1)
+    dfs(f"{cf}+{num}", num + 1)
+    dfs(f"{cf}-{num}", num + 1)
 
+t = int(input())
 for _ in range (t):
   n = int(input())
-  arr = []
-  x = 1
-  for i in range (2*n - 1):
-    if i % 2 == 0:
-      arr.append(str(x))
-      x += 1
-    else:
-      arr.append('')
-  
-  ei = [i for i in range (1, 2*n - 1, 2)] # index where operator should be in
-  coo = list(itertools.product(co, repeat=n-1)) # combination of operators
-  
-  for c in coo:
-    for i, op in zip(ei, c): # change operator
-      arr[i] = op
-      
-    if eval(''.join(arr)) == 0:
-      for j in range (1, 2*n - 1, 2):
-        if arr[j] == '': arr[j] = ' '
-
-      print(''.join(arr))
-  
+  dfs("1", 2)
   print()
