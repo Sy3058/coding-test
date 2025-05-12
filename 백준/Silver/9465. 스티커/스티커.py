86c6de5943566fe38cdbde6d396753b0c39a8120
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-05-13 03:23:13 +0900

    [Silver I] Title: 스티커, Time: 624 ms, Memory: 49404 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range (t):
+  n = int(input())
+  arr = [list(map(int, input().split())) for _ in range (2)]
+
+  # dp 배열
+  dp = [[0] * n for _ in range (2)]
+
+  # 스티커 길이가 1인 경우
+  dp[0][0] = arr[0][0]
+  dp[1][0] = arr[1][0]
+
+  if n == 1:
+    print(max(dp[0][0], dp[1][0]))
+    continue
+
+  # 스티커 길이가 2인 경우
+  dp[0][1] = arr[1][0] + arr[0][1]
+  dp[1][1] = arr[0][0] + arr[1][1]
+
+  if n == 2:
+    print(max(dp[0][1], dp[1][1]))
+    continue
+
+  # 그 이상인 경우
+  for i in range (2, n):
+    dp[0][i] = max(dp[1][i-2], dp[1][i-1]) + arr[0][i]
+    dp[1][i] = max(dp[0][i-2], dp[0][i-1]) + arr[1][i]
+  
+  print(max(dp[0][-1], dp[1][-1]))
