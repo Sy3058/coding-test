1622f59fece30dea1dbf5399ca60ac7780c3b66f
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-07-02 01:10:59 +0900

    [Gold IV] Title: 스도쿠, Time: 4112 ms, Memory: 140216 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -0,0 +1,47 @@
+import sys
+input = sys.stdin.readline
+
+board = []
+blank = []
+for i in range (9):
+  s = input().strip()
+  sti = []
+  for j in range (9):
+    c = int(s[j])
+    sti.append(c)
+    if c == 0:
+      blank.append((i, j))
+  board.append(sti)
+
+def check(x, y, target):
+  for i in range (9):
+    if board[x][i] == target: # 가로줄에 target이 이미 존재하는 경우
+      return False
+    
+    if board[i][y] == target: # 세로줄에 target이 이미 존재하는 경우
+      return False
+  
+  # 3*3 범위
+  xr = 3 * (x//3)
+  yr = 3 * (y//3)
+  for i in range (xr, xr + 3):
+    for j in range (yr, yr + 3):
+      if board[i][j] == target:
+        return False
+  
+  return True
+  
+def fill(idx):
+  if idx == len(blank):
+    for i in range (9):
+      print(''.join(map(str, board[i])))
+    exit()
+  
+  x, y = blank[idx]
+  for i in range (1, 10):
+    if check(x, y, i):
+      board[x][y] = i
+      fill(idx + 1)
+      board[x][y] = 0
+
+fill(0)
