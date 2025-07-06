6696ff72312199ca0a633bc293d2a6208a23fbd1
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-07-07 02:44:14 +0900

    [Bronze I] Title: 배고파(Easy), Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.py
+++ 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.py
@@ -2,14 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+powers = [1 << i for i in range (61)] # 2^i 리스트
+
 for _ in range (n):
-  flag = False
   m = int(input())
   for i in range (61):
-    for j in range (i, 61):
-      if 2**i + 2**j == m:
-        print(i, j)
-        flag = True
-        break
-    if flag:
+    rem = m - powers[i]
+    if rem <= 0: # 더이상 검사할 필요 없음
+      continue
+
+    if (rem & (rem - 1)) == 0: # rem이 2의 거듭 제곱이면 ex) rem = 1000(2) rem - 1 = 111(2) 이므로 rem & (rem - 1) = 0이 됨
+      j = rem.bit_length() - 1
+      print(min(i, j), max(i, j))
       break
