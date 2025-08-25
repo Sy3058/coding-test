ea5242e7514bc2dd014cc8ac23258ea9998959c3
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-08-25 15:25:29 +0900

    [Gold V] Title: ABCDE, Time: 708 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,33 +1,36 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
+def dfs(node, depth):
+    global flag
+    if flag:  # 이미 찾았으면 중단
+        return
+    if depth == 5:  # 친구 관계 5명 연속 발견
+        flag = True
+        return
+
+    for nxt in graph[node]:
+        if not visited[nxt]:
+            visited[nxt] = True
+            dfs(nxt, depth + 1) # 백트래킹
+            visited[nxt] = False
+
 n, m = map(int, input().split())
-friend = {i:[] for i in range (n)}
+graph = [[] for _ in range(n)]
 visited = [False] * n
 flag = False
-route = []
 
-for _ in range (m):
-  a, b = map(int, input().split())
-  friend[a].append(b)
-  friend[b].append(a)
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-def dfs(s, depth):
-  global flag
-  if depth == 5:
-    flag = True
-    return
+for i in range(n):
+    if flag:
+        break
+    visited[i] = True
+    dfs(i, 1)  # 시작 노드도 depth=1부터 카운트
+    visited[i] = False
 
-  nv = friend[s]
-
-  for x in nv:
-    if not visited[x]:
-      visited[x] = True
-      dfs(x, depth + 1)
-      visited[x] = False
-
-for i in range (n):
-  if not flag:
-    dfs(i, 0)
-
-print(1) if flag else print(0)
+print(1 if flag else 0)
