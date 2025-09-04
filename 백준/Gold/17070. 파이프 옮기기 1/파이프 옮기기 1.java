2054bc3e050ef3ecff605e6b9d28c6395dea11b3
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-09-04 15:00:56 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.util.List;
+import java.io.*;
+import java.awt.*;
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int t, n;
+	static int[][] graph = new int[17][17];
+	static int[][][] dp = new int[3][17][17];
+	static int ans = 0;
+
+	public static void main(String[] args) throws Exception {
+
+		t = 1;
+		for (int tc = 1; tc <= t; tc++) {
+
+			// init
+
+			// input
+			n = Integer.parseInt(br.readLine());
+			for (int i = 1; i <= n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 1; j <= n; j++) {
+					graph[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			// logic
+
+			dp[0][1][2] = 1;
+			for (int i = 1; i <= n; i++) {
+				for (int j = 3; j <= n; j++) {
+					if (graph[i][j] == 1)
+						continue;
+
+					dp[0][i][j] = dp[0][i][j - 1] + dp[1][i][j - 1];
+					dp[2][i][j] = dp[2][i - 1][j] + dp[1][i - 1][j];
+					if (graph[i - 1][j] == 0 && graph[i][j - 1] == 0) {
+						dp[1][i][j] = dp[0][i - 1][j - 1] + dp[1][i - 1][j - 1] + dp[2][i - 1][j - 1];
+					}
+
+				}
+			}
+
+
+			ans = dp[0][n][n] + dp[1][n][n] + dp[2][n][n];
+			// output
+//			bw.append("#").append(Integer.toString(tc)).append(" ");
+			bw.append(Integer.toString(ans));
+//			bw.append("\n");
+			bw.flush();
+		}
+
+	}
+}
