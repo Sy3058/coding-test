e570619d39311ebd8f810d036b9bc7de8b3040a1
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-09-08 12:23:10 +0900

    [D3] Title: 0/1 Knapsack, Time: 116 ms, Memory: 33,000 KB -BaekjoonHub

---
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+  static int N, K;
+  static List<int[]> items;
+  static int [][] dp;
+  
+  public static void main (String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
+
+    for (int tc = 1; tc <= T; tc++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      N = Integer.parseInt(st.nextToken());
+      K = Integer.parseInt(st.nextToken());
+
+      items = new ArrayList<>();
+      items.add(new int [] {0, 0}); // 계산 편의를 위한 더미 데이터
+      dp = new int [N+1][K+1];
+
+      for (int i = 0; i < N; i++) {
+        st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken()); // 부피
+        int c = Integer.parseInt(st.nextToken()); // 가치
+        items.add(new int[] {v, c});
+      }
+
+      for (int i = 1; i <= N; i++) { // 확인중인 item
+        for (int j = 1; j <= K; j++) { // 부피 제한
+          int v = items.get(i)[0];
+          int c = items.get(i)[1];
+
+          // 부피제한보다 무게가 크면 넣을 수 없음
+          if (j < v) dp[i][j] = dp[i-1][j];
+          // 현재 물건을 넣는것과 넣지 않는 것 중 가치가 더 높은 걸 저장
+          else dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-v] + c);
+        }
+      }
+      sb.append("#").append(tc).append(" ").append(dp[N][K]).append("\n");
+    }
+    System.out.print(sb);
+  }
+}
