a62b994169fd5c891228d8ebfb3d377f0b004be8
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-09-08 12:29:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 204 ms, Memory: 53988 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -2,41 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+  static int N, K;
+  static List<int[]> items;
+  static int [][] dp;
+  
+  public static void main (String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-  static StringBuilder ansStr = new StringBuilder();
-  static StringTokenizer st;
-  static int n, k;
-  static int[][] thing = new int[101][2];
-  static int[] dp = new int[100001];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
 
+    items = new ArrayList<>();
+    items.add(new int [] {0, 0}); // 계산 편의를 위한 더미 데이터
+    dp = new int [N+1][K+1];
 
-  public static void main(String[] args) throws Exception {
-    // input
-    st = new StringTokenizer(br.readLine());
-    n = Integer.parseInt(st.nextToken());
-    k = Integer.parseInt(st.nextToken());
-    for (int i = 1; i <= n; i++) {
-        st = new StringTokenizer(br.readLine());
-        int w = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
-        thing[i][0] = w; thing[i][1] = v;
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int v = Integer.parseInt(st.nextToken()); // 부피
+      int c = Integer.parseInt(st.nextToken()); // 가치
+      items.add(new int[] {v, c});
     }
-    // input end
 
-    //logic start
-    for (int i = 1; i <= n; i++) {
-      for (int j = k; j >= 0; j--) {
-        if (j - thing[i][0] >= 0) dp[j] = Math.max(dp[j], dp[j - thing[i][0]] + thing[i][1]);
+    for (int i = 1; i <= N; i++) { // 확인중인 item
+      for (int j = 1; j <= K; j++) { // 부피 제한
+        int v = items.get(i)[0];
+        int c = items.get(i)[1];
+
+        // 부피제한보다 무게가 크면 넣을 수 없음
+        if (j < v) dp[i][j] = dp[i-1][j];
+        // 현재 물건을 넣는것과 넣지 않는 것 중 가치가 더 높은 걸 저장
+        else dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-v] + c);
       }
-//      for (int j = 1; j <= k; j++) System.out.print(dp[j] + " ");
-//      System.out.println();
     }
-
-
-    ansStr.append(dp[k]);
-    bw.write(ansStr.toString());
-    bw.flush();
+    System.out.println(dp[N][K]);
   }
 }
