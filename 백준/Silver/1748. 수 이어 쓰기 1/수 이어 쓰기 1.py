a71f7c59cf793da46f57978760fd18f00ebb1f19
Sy3058 <156003765+Sy3058@users.noreply.github.com>
2025-11-09 23:12:47 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -1,8 +1,14 @@
-n = input()
-ans = 0
-for i in range (1, len(n)):
-  ans += 9 * (10 ** (i - 1)) * i
+import sys
+import math
+input = sys.stdin.readline
 
-# 남은 자리 추가
-ans += (int(n) - 10 ** (len(n) - 1) + 1) * len(n)
+# 1 ~ 9까지는 1개(log10), 10 ~ 99까지는 2개(log100) ... 개씩 자릿수가 늘어남
+n = int(input())
+tmp = int(math.log10(n)) + 1
+
+ans = 0
+for i in range (1, tmp):
+  ans += 9 * (10 ** (i-1)) * i # 자릿수마다 9 * (10 ** 자릿수) 개씩 늘어남
+# 마지막 자리수에선 아래에 해당하는 수만큼 추가됨
+ans += (n - 10 ** (tmp - 1) + 1) * tmp
 print(ans)
